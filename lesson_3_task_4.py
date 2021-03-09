74400b03669a12f3fa870c5622b8f095b2558cd8
Dmitrii Rodygin <DRodygin2@slb.com>
2021-03-09 16:21:00 +0300

    'lesson_task_4'

---
+++ lesson_3_task_4.py
@@ -0,0 +1,23 @@
+# Определить, какое число в массиве встречается чаще всего.
+
+import random
+
+LENGTH = 20
+MIN_VALUE = 0
+MAX_VALUE = 1000
+array = [random.randint(MIN_VALUE, MAX_VALUE) for i in range(LENGTH)]
+print(f'Начальный массив: {array}')
+num = array[0]
+freq = 1
+for i in range(len(array)):
+    egg = 1
+    for j in range(i+1, len(array)):
+        if array[i] == array[j]:
+            egg += 1
+    if egg > freq:
+        freq = egg
+        num = array[i]
+if freq > 1:
+    print(f'Число {num} встречается {freq} раз')
+else:
+    print("Все элементы массива уникальны")
