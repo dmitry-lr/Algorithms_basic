880018d454961c687cf95f266dd3ecee9e4a14e5
Dmitrii Rodygin <DRodygin2@slb.com>
2021-03-23 18:42:14 +0300

    'lesson_7_task_1_with_description'

---
+++ lesson_7_task_1.py
@@ -0,0 +1,30 @@
+# Отсортируйте по убыванию методом пузырька одномерный целочисленный массив,
+# заданный случайными числами на промежутке [-100; 100).
+# Выведите на экран исходный и отсортированный массивы.
+
+import random
+
+
+def find_smallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def selection_sort(arr):
+    new_arr = []
+    for i in range(len(arr)):
+        smallest = find_smallest(arr)
+        new_arr.append(arr.pop(smallest))
+    return new_arr
+
+
+SIZE = 10
+LIMIT = 100
+init_arr = [random.randrange(-LIMIT, LIMIT) for i in range(SIZE)]
+print(init_arr)
+print(selection_sort(init_arr))
